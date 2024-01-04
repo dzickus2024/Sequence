b393b3c202afaa335e518b8728bf6e31ac9c8775
Dylan Zickus <dzickus2024@student.amsacs.org>
2024-01-04 16:27:25 -0500

    push?

--- Tile.java
+++ Tile.java
@@ -1,9 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Tile {
+
+public class Tile extends Frame implements ActionListener{
 	private String cardName;
+	private Button button;
 	
 	public Tile(String cardName) {
+		super(cardName);
+
 		this.cardName = cardName;
+
+		button = new Button(this.cardName);
+		add(button);
+		button.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		//do stuff
 	}
 	
 	public String getCardName() {
