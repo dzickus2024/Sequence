845876767b31489f34a951c255fa13534a506904
Dylan Zickus <dzickus2024@student.amsacs.org>
2024-01-04 15:42:43 -0500

    color instead

--- Player_Human.java
+++ Player_Human.java
@@ -1,6 +1,8 @@
+import java.awt.Color;
+
 
 public class Player_Human extends Player {
-	public Player_Human(String name, String color) {
+	public Player_Human(String name, Color color) {
 		super(name, color);
 	}
 }
